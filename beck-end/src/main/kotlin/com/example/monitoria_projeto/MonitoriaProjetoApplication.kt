f1cdf8054d92e9ba50860469e113c2852424e639
Domiciano <guilhermedomicianosilva@gmail.com>
2025-01-21 12:51:34 -0300

    first commit

---
+++ beck-end/src/main/kotlin/com/example/monitoria_projeto/MonitoriaProjetoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.monitoria_projeto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MonitoriaProjetoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MonitoriaProjetoApplication>(*args)
+}
---
+++ beck-end/src/test/kotlin/com/example/monitoria_projeto/MonitoriaProjetoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.monitoria_projeto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MonitoriaProjetoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
